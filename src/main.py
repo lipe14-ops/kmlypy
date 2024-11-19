f08421a8df633428ed340e2793934a9b9c88ecad
Filipe Soares <filipe.ns1001@gmail.com>
2024-11-19 15:22:33 -0300

    chorus: tree folder system parsing

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 class KMLTag(ABC):
-    def __init__(self, **kwargs):
+    def __init__(self, namespace: str | None = '', **kwargs):
+        self.namespace = namespace
         self.__attributes = kwargs
         self.children: list[KMLTag] = []
 
@@ -29,44 +30,72 @@ class KMLTag(ABC):
 
 
 class KMLFolder(KMLTag):
-    def __init__(self, name: str | None = None, description: str | None = None):
-        super().__init__(name=name, description=description)
+    def __init__(self, namespace: str | None = '', name: str | None = None, description: str | None = None):
+        super().__init__(namespace, name=name, description=description)
         self.name = name
         self.description = description
         self.children: list[KMLTag] = []
 
 class KMLDocument(KMLTag):
-    def __init__(self, name: str | None = None, description: str | None = None):
-        super().__init__(name=name, description=description)
+    def __init__(self, namespace: str | None = '', name: str | None = None, description: str | None = None):
+        super().__init__(namespace, name=name, description=description)
         self.name = name
         self.description = description
         self.children: list[KMLTag] = []
 
 
-class KMLFileTree(object):
-    def __init__(self):
-        self.children: list[KMLTag] = []
+def kml_file_explore(tree_root, tag: KMLTag) -> KMLTag:
 
+    for child in tree_root:
 
-def parse_kml_file(filepath: Path | str) -> KMLFileTree:
-    ...
+        element: KMLTag | None = None
+
+        match child.tag:
+            case x if x.endswith('Folder') or x.endswith('Document'):
+
+                container_name = child.find(f'{tag.namespace}name')
+                container_description = child.find(f'{tag.namespace}description')
+
+                kml_container_type = KMLFolder if x.endswith('Folder') else KMLDocument
+                    
+                element = kml_container_type(
+                    namespace   = tag.namespace,
+                    name        = container_name.text if container_name is not None else None,
+                    description = container_description.text if container_description is not None else None
+                )
+
+            case _:
+                kml_file_explore(child, tag)
+
+        if not element:
+            continue
+
+        tag._add_tag(element)
+        kml_file_explore(child, element)
+
+    return tag
+
 
 class KMLFile(object):
     def __init__(self, filepath: Path | str) -> None:
         self.filepath = filepath
 
     def __enter__(self):
-        return parse_kml_file(self.filepath)
+        kml_tree = ET.parse(self.filepath)
+        kml_root = kml_tree.getroot()
+
+        namespace = kml_root.tag[: kml_root.tag.index('}') + 1]
+
+        document = KMLDocument(namespace=namespace)
+        return kml_file_explore(kml_root, document).children.pop()
 
     def __exit__(self, exec_type, exec_val, exec_tb) -> None:
         return 
         
 def main():
-    folder1 = KMLFolder(name='FIRST FOLDER')
-    folder2 = KMLFolder()
 
-    folder1._add_tag(folder2)
-    print(folder1)
+    with KMLFile('./res/file-3.kml') as kml_file:
+        print(kml_file)
 
     return
 
